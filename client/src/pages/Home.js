ead3013509836d91c5a65f83ace57296f3090dd8
AntoniosProvidakis <ant.providakis@gmail.com>
2019-02-27 00:12:12 +0200

    Implement Home page with temporary movie grids

---
+++ client/src/pages/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+import MoviesGrid from '../components/MoviesGrid';
+import '../styles/Home.css';
+import * as moviesAPI from '../api/moviesAPI';
+
+function Home(props) {
+    const movies = moviesAPI.all();
+
+    return (
+        <div className="Home">
+            <Header
+                className='Home__welcome-message'
+                size='huge'
+                textAlign='center'
+                dividing
+            >
+                Welcome to Movie Finder
+                <Header.Subheader>
+                    Find information about any movie or actor/actress.
+                </Header.Subheader>
+            </Header>
+
+            <div className="Home__in-theaters">
+                <MoviesGrid
+                    movies={movies}
+                    title='Movies In Theaters'
+                    tabletColumnWidthPerRow={4}
+                />
+            </div>
+
+            <div className="Home__upcoming">
+                <MoviesGrid
+                    movies={movies}
+                    title='Upcoming Movies'
+                    tabletColumnWidthPerRow={4}
+                />
+            </div>
+
+            <div className="Home__popular">
+                <MoviesGrid
+                    movies={movies}
+                    title='Popular Movies'
+                    tabletColumnWidthPerRow={4}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Home;
