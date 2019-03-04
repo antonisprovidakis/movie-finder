1d7b4a39d890fa2c3239af098b7b656a861c7b7b
AntoniosProvidakis <ant.providakis@gmail.com>
2019-03-05 01:49:58 +0200

    Implement a simple NotFound page

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+
+    return (
+        <div className="NotFound">
+            <h1>Page not found.</h1>
+        </div>
+    );
+}
+
+export default NotFound;
