eb24194001bac865af286968e1f7e6841ea6e53a
AntoniosProvidakis <ant.providakis@gmail.com>
2019-03-06 13:16:53 +0200

    Install dotenv for loading environmental variables

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const port = process.env.PORT || 8080;
 
+const port = process.env.PORT || 8080;
 const isInProduction = process.env.NODE_ENV === 'production';
 
 // Put all API endpoints under '/api'
 app.get('/api/hello', (req, res) => {
-  res.json({data: 'hello world'});
+  res.json({ data: 'hello world' });
 });
 
 if (isInProduction) {
@@ -16,14 +16,14 @@ if (isInProduction) {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 else {
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.send('Server is used only as an API server in dev mode.');
   });
 }
 
-app.listen(port, ()=> console.log(`Server started on port: ${port}`));
+app.listen(port, () => console.log(`Server started on port: ${port}`));
