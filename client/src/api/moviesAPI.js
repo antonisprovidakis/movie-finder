05f313f92f1025976ee7af2e9808e845a159d0ef
AntoniosProvidakis <ant.providakis@gmail.com>
2019-03-08 13:36:53 +0200

    Remove mock data and api

--- client/src/api/moviesAPI.js
+++
@@ -1,9 +0,0 @@
-import { movies } from './data/movies';
-
-export async function all() {
-    return movies;
-}
-
-export async function get(id) {
-    return movies.find(movie => movie.id === id);
-}
--- client/src/api/peopleAPI.js
+++
@@ -1,9 +0,0 @@
-import { people } from './data/people';
-
-export async function all() {
-    return people;
-}
-
-export async function get(id) {
-    return people.find(person => person.id === id);
-}
