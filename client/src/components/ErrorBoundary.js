708a865f14fe30b89789f877cdc55e88127aa5ac
AntoniosProvidakis <ant.providakis@gmail.com>
2019-03-18 16:28:38 +0200

    Implement a basic Error Boundary component

---
+++ client/src/components/ErrorBoundary.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+        };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { error };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // You can log error messages to an error reporting service here
+    }
+
+    render() {
+        if (this.state.error) {
+            // Error path
+            return (
+                <div>
+                    <h2>Something went wrong.</h2>
+                    {this.state.error && this.state.error.toString()}
+                </div>
+            );
+        }
+        // Normally, just render children
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
