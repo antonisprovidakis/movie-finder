01a604344efa914cf710c6df2b0db8399037de42
AntoniosProvidakis <ant.providakis@gmail.com>
2019-03-23 23:45:33 +0200

    Implement MoviesGridPlacehodler component

---
+++ client/src/components/MoviesGridPlaceholder.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Grid, Header, Menu } from 'semantic-ui-react';
+import PosterMovieCardPlaceholder from './PosterMovieCardPlaceholder';
+import times from 'lodash/times';
+
+function MoviesGridPlaceholder({
+    title = '',
+    num,
+    ...rest
+}) {
+    return (
+        <div className='MoviesGridPlaceholder'>
+            {title.length > 0 &&
+                <Menu secondary>
+                    <Menu.Item fitted>
+                        <Header as='h2' className="MoviesGridPlaceholder__title">
+                            {title}
+                        </Header>
+                    </Menu.Item>
+                </Menu>
+            }
+
+            <Grid
+                className='MoviesGridPlaceholder__movies'
+                {...rest}
+            >
+                {times(num, (index) =>
+                    <Grid.Column
+                        className='MoviesGridPlaceholder__column'
+                        key={index}
+                    >
+                        <PosterMovieCardPlaceholder />
+                    </Grid.Column>
+                )}
+            </Grid>
+        </div>
+    );
+}
+
+export default MoviesGridPlaceholder;
