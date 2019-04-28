8437a32ed5f065f7a10b2cd0728da765cf419ee8
AntoniosProvidakis <ant.providakis@gmail.com>
2019-04-28 22:20:50 +0300

    Add default options from api calls

---
+++ client/src/api/defaults/index.js
@@ -0,0 +1,25 @@
+const movie = {
+    language: 'en-US'
+};
+
+const movieCollection = {
+    page: 1,
+    language: 'en-US',
+    region: 'US'
+};
+
+const person = {
+    language: 'en-US'
+};
+
+const personCollection = {
+    page: 1,
+    language: 'en-US'
+};
+
+export const options = {
+    movie,
+    movieCollection,
+    person,
+    personCollection
+};
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,10 +2,12 @@ import * as movieAPI from './movieAPI';
 import * as personAPI from './personAPI';
 import * as searchAPI from './searchAPI';
 import * as configurationAPI from './configurationAPI';
+import { options as defaultOptions } from './defaults';
 
 export {
     movieAPI,
     personAPI,
     searchAPI,
     configurationAPI,
+    defaultOptions
 }
