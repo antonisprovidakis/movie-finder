73265bc797521c4c609f3339e9cf0a7ff44baa65
AntoniosProvidakis <ant.providakis@gmail.com>
2019-04-29 19:23:45 +0300

    Clear redux actions from using default method options, Also remove them from codebase

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,12 +1,11 @@
 import Schemas from '../schemas';
-import { defaultOptions, movieAPI, personAPI, } from '../../api';
+import { movieAPI, personAPI, } from '../../api';
 
 const LOAD_MOVIE_INFO_REQUEST = 'LOAD_MOVIE_INFO_REQUEST';
 const LOAD_MOVIE_INFO_SUCCESS = 'LOAD_MOVIE_INFO_SUCCESS';
 const LOAD_MOVIE_INFO_FAILURE = 'LOAD_MOVIE_INFO_FAILURE';
 
-export function loadMovieInfo(movieId, requiredFields = [], options = defaultOptions.movie) {
-    options = { ...defaultOptions.movie, ...options };
+export function loadMovieInfo(movieId, requiredFields = [], options = {}) {
     return {
         types: [LOAD_MOVIE_INFO_REQUEST, LOAD_MOVIE_INFO_SUCCESS, LOAD_MOVIE_INFO_FAILURE],
         payload: { movieId, options },
@@ -28,8 +27,7 @@ const LOAD_MOVIES_BY_CATEGORY_REQUEST = 'LOAD_MOVIES_BY_CATEGORY_REQUEST';
 const LOAD_MOVIES_BY_CATEGORY_SUCCESS = 'LOAD_MOVIES_BY_CATEGORY_SUCCESS';
 const LOAD_MOVIES_BY_CATEGORY_FAILURE = 'LOAD_MOVIES_BY_CATEGORY_FAILURE';
 
-export function loadMoviesByCategory(category, options = defaultOptions.movieCollection) {
-    options = { ...defaultOptions.movieCollection, ...options };
+export function loadMoviesByCategory(category, options = {}) {
     return {
         types: [LOAD_MOVIES_BY_CATEGORY_REQUEST, LOAD_MOVIES_BY_CATEGORY_SUCCESS, LOAD_MOVIES_BY_CATEGORY_FAILURE],
         payload: { category, options },
@@ -58,8 +56,7 @@ const DISCOVER_MOVIES_REQUEST = 'DISCOVER_MOVIES_REQUEST';
 const DISCOVER_MOVIES_SUCCESS = 'DISCOVER_MOVIES_SUCCESS';
 const DISCOVER_MOVIES_FAILURE = 'DISCOVER_MOVIES_FAILURE';
 
-export function discoverMovies(options = defaultOptions.movieCollection) {
-    options = { ...defaultOptions.movieCollection, ...options };
+export function discoverMovies(options = {}) {
     // TODO: turn this into a helper function
     // or use query onto payload?
     const query = `${options.primary_release_year}-${options.sort_by}-${options.with_genres.join('_')}`;
@@ -82,8 +79,7 @@ const LOAD_PERSON_INFO_REQUEST = 'LOAD_PERSON_INFO_REQUEST';
 const LOAD_PERSON_INFO_SUCCESS = 'LOAD_PERSON_INFO_SUCCESS';
 const LOAD_PERSON_INFO_FAILURE = 'LOAD_PERSON_INFO_FAILURE';
 
-export function loadPersonInfo(personId, requiredFields = [], options = defaultOptions.person) {
-    options = { ...defaultOptions.person, ...options };
+export function loadPersonInfo(personId, requiredFields = [], options = {}) {
     return {
         types: [LOAD_PERSON_INFO_REQUEST, LOAD_PERSON_INFO_SUCCESS, LOAD_PERSON_INFO_FAILURE],
         payload: { personId, options },
@@ -105,8 +101,7 @@ const LOAD_POPULAR_PERSONS_REQUEST = 'LOAD_POPULAR_PERSONS_REQUEST';
 const LOAD_POPULAR_PERSONS_SUCCESS = 'LOAD_POPULAR_PERSONS_SUCCESS';
 const LOAD_POPULAR_PERSONS_FAILURE = 'LOAD_POPULAR_PERSONS_FAILURE';
 
-export function loadPopularPersons(options = defaultOptions.personCollection) {
-    options = { ...defaultOptions.personCollection, ...options };
+export function loadPopularPersons(options = {}) {
     return {
         types: [LOAD_POPULAR_PERSONS_REQUEST, LOAD_POPULAR_PERSONS_SUCCESS, LOAD_POPULAR_PERSONS_FAILURE],
         payload: { options },
