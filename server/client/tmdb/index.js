f2c370c0418faa44fa74a3f00d7519b5f6ba02e0
AntoniosProvidakis <ant.providakis@gmail.com>
2019-05-08 00:22:56 +0300

    Server: implement rate limiting on tmdb client (per tmdb API instructions), using limits.js

--- server/client/tmdb/index.js
+++ server/client/tmdb/index.js
@@ -1,5 +1,11 @@
 const axios = require('axios');
 const qs = require('qs');
+const limits = require('limits.js')
+
+const MAX_REQUESTS = 40;
+const TIME_FRAME = 10 * 1000; // 10 seconds
+const SAFE_TIME_WINDOW = 200; // just to be safe
+const rateLimiter = limits().within(TIME_FRAME + SAFE_TIME_WINDOW, MAX_REQUESTS);
 
 const tmdb = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
@@ -8,6 +14,12 @@ const tmdb = axios.create({
     },
     paramsSerializer: params => qs.stringify(params, { arrayFormat: 'comma' })
 });
+
+tmdb.interceptors.request.use(config =>
+    new Promise(resolve => {
+        rateLimiter.push(() => resolve(config));
+    })
+);
 
 async function get(endpoint, options = {}) {
     const res = await tmdb.get(endpoint, { params: options });
