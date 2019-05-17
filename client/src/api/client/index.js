33476e35f6a00be8f4d327442f87eb0f17126868
AntoniosProvidakis <ant.providakis@gmail.com>
2019-05-18 01:53:03 +0300

    Client: Use "qs" to serialize params in correct format

--- client/src/api/client/index.js
+++ client/src/api/client/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import qs from 'qs';
 
-const client = axios.create({ baseURL: '/api', });
+const client = axios.create({
+    baseURL: '/api',
+    paramsSerializer: params => qs.stringify(params, { arrayFormat: 'comma' })
+});
 
 async function get(endpoint, options = {}) {
     const res = await client.get(endpoint, { params: options });
