f876539d378cdfa47899c34caa29c95f6e4470cb
AntoniosProvidakis <ant.providakis@gmail.com>
2019-05-18 02:22:47 +0300

    Implement reducers to handle resource (movie, person) fetching state

--- client/src/redux/reducers/ui/index.js
+++ client/src/redux/reducers/ui/index.js
@@ -10,8 +10,32 @@ const movieCardViewStyle = (state = 'poster', action) => {
     }
 }
 
+const createIsFetchingResourceReducer = (request, success, failure) => {
+    return (state = false, action) => {
+        switch (action.type) {
+            case request:
+                return true;
+            case success:
+            case failure:
+                return false;
+            default:
+                return state;
+        }
+    }
+}
+
 const ui = combineReducers({
-    movieCardViewStyle
+    movieCardViewStyle,
+    isFetchingMovieInfo: createIsFetchingResourceReducer(
+        ActionTypes.LOAD_MOVIE_INFO_REQUEST,
+        ActionTypes.LOAD_MOVIE_INFO_SUCCESS,
+        ActionTypes.LOAD_MOVIE_INFO_FAILURE
+    ),
+    isFetchingPersonInfo: createIsFetchingResourceReducer(
+        ActionTypes.LOAD_PERSON_INFO_REQUEST,
+        ActionTypes.LOAD_PERSON_INFO_SUCCESS,
+        ActionTypes.LOAD_PERSON_INFO_FAILURE
+    ),
 });
 
 export default ui;
