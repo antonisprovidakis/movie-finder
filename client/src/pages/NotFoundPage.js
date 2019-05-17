e1d9c9178b78d2d30392c2823ff25651bcd19f5e
AntoniosProvidakis <ant.providakis@gmail.com>
2019-05-18 02:12:11 +0300

    Reuse NotFound component in NotFoundPage

--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
-import '../styles/NotFoundPage.css';
+import NotFound from '../components/NotFound';
 
 function NotFoundPage() {
-    return (
-        <div className="NotFoundPage" >
-            <h1>Oops!—We can't find the page you're looking for.</h1>
-            <Button primary as={Link} to='/'>Go back to Home</Button>
-        </div>
-    );
+    return <NotFound />
 }
 
 export default NotFoundPage;
