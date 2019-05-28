c504a960aad1604002a66279594e4b6342a15801
AntoniosProvidakis <ant.providakis@gmail.com>
2019-05-29 02:40:04 +0300

    Implement Loader component

---
+++ client/src/components/Loader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { Loader as LoaderSUI } from 'semantic-ui-react';
+
+function Loader({ text }) {
+    return (
+        <LoaderSUI
+            className='Loader'
+            active
+            inline='centered'
+        >
+            {text}
+        </LoaderSUI>
+    );
+}
+
+Loader.propTypes = {
+    text: PropTypes.string,
+}
+
+Loader.defaultProps = {
+    text: ''
+}
+
+export default Loader;
