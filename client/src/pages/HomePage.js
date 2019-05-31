14280a8bd569ae31e980ecf57e047a6620c7b9a0
AntoniosProvidakis <ant.providakis@gmail.com>
2019-06-01 01:46:32 +0300

    MoviePage: Refactor (naming) + Pass 3 movies groups in seperate instead of a single object

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,26 +10,33 @@ import '../styles/HomePage.css';
 import PosterMovieCard from '../components/PosterMovieCard';
 import PosterMovieCardPlaceholder from '../components/PosterMovieCardPlaceholder';
 
-function HomePage({ movies, loadMoviesByCategory }) {
+function HomePage({
+    popularMovies,
+    inTheatersMovies,
+    upcomingMovies,
+    loadMoviesByCategory
+}) {
     useEffect(() => {
         const categories = ['popular', 'in-theaters', 'upcoming'];
-        categories.forEach(category => loadMoviesByCategory(category, { page: 1, region: 'US' }));
+        categories.forEach(category =>
+            loadMoviesByCategory(category, { page: 1, region: 'US' })
+        );
     }, [loadMoviesByCategory]);
 
     const sectionsData = [
         {
             title: 'Popular Movies',
-            movies: movies.popularMovies,
+            movies: popularMovies,
             linkTo: '/movie/popular'
         },
         {
             title: 'Movies In Theaters',
-            movies: movies.inTheatersMovies,
+            movies: inTheatersMovies,
             linkTo: '/movie/in-theaters'
         },
         {
             title: 'Upcoming Movies',
-            movies: movies.upcomingMovies,
+            movies: upcomingMovies,
             linkTo: '/movie/upcoming'
         }
     ];
@@ -109,24 +116,24 @@ const mapStateToProps = (state) => {
     const upcomingMovieIds = _get(upcomingMoviesSubTree, path, []).slice(0, 4);
     const popularMovieIds = _get(popularMoviesSubTree, path, []).slice(0, 4);
 
-    const movies = {
-        inTheatersMovies: inTheatersMovieIds.map(id => cachedMovies[id]),
-        upcomingMovies: upcomingMovieIds.map(id => cachedMovies[id]),
-        popularMovies: popularMovieIds.map(id => cachedMovies[id]),
-    };
+    const popularMovies = popularMovieIds.map(id => cachedMovies[id]);
+    const inTheatersMovies = inTheatersMovieIds.map(id => cachedMovies[id]);
+    const upcomingMovies = upcomingMovieIds.map(id => cachedMovies[id]);
 
     return {
-        movies
+        popularMovies,
+        inTheatersMovies,
+        upcomingMovies
     }
 }
 
 HomePage.propTypes = {
-    movies: PropTypes.shape({
-        popularMovies: PropTypes.arrayOf(PropTypes.object).isRequired,
-        inTheatersMovies: PropTypes.arrayOf(PropTypes.object).isRequired,
-        upcomingMovies: PropTypes.arrayOf(PropTypes.object).isRequired
-    }).isRequired,
+    popularMovies: PropTypes.arrayOf(PropTypes.object).isRequired,
+    inTheatersMovies: PropTypes.arrayOf(PropTypes.object).isRequired,
+    upcomingMovies: PropTypes.arrayOf(PropTypes.object).isRequired,
     loadMoviesByCategory: PropTypes.func.isRequired
 }
 
-export default connect(mapStateToProps, { loadMoviesByCategory })(HomePage);
+export default connect(mapStateToProps, {
+    loadMoviesByCategory
+})(HomePage);
