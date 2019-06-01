adb2122e6d9277c1ed89412f5a5079bb39a60c03
AntoniosProvidakis <ant.providakis@gmail.com>
2019-06-01 21:02:37 +0300

    Client - API: Freeze sortingFilters

--- client/src/api/config/sortingFilters.js
+++ client/src/api/config/sortingFilters.js
@@ -1,8 +1,8 @@
-export const sortingFilters = [
+export const sortingFilters = Object.freeze([
     { id: 'popularity.desc', text: 'Popular Descending' },
     { id: 'popularity.asc', text: 'Popular Ascending' },
     { id: 'vote_average.desc', text: 'Rating Descending' },
     { id: 'vote_average.asc', text: 'Rating Ascending' },
     { id: 'release_date.desc', text: 'Release Date Descending' },
     { id: 'release_date.asc', text: 'Release Date Ascending' }
-];
+]);
