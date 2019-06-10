a944c76664cfe634b1471fc2b20233fbbba57b5c
AntoniosProvidakis <ant.providakis@gmail.com>
2019-06-10 17:50:49 +0300

    Implement tests for extractReleaseDatesForRegion.js

---
+++ client/src/utils/__tests__/extractReleaseDatesForRegion.js
@@ -0,0 +1,450 @@
+import extractReleaseDatesForRegion from '../extractReleaseDatesForRegion';
+
+const movie = {
+  adult: false,
+  backdrop_path: '/phxiKFDvPeQj4AbkvJLmuZEieDU.jpg',
+  belongs_to_collection: null,
+  budget: 200000000,
+  genres: [
+    {
+      id: 878,
+      name: 'Science Fiction'
+    },
+    {
+      id: 28,
+      name: 'Action'
+    }
+  ],
+  homepage: 'http://darkphoenix.com',
+  id: 320288,
+  imdb_id: 'tt6565702',
+  original_language: 'en',
+  original_title: 'Dark Phoenix',
+  overview:
+    "The X-Men face their most formidable and powerful foe when one of their own, Jean Grey, starts to spiral out of control. During a rescue mission in outer space, Jean is nearly killed when she's hit by a mysterious cosmic force. Once she returns home, this force not only makes her infinitely more powerful, but far more unstable. The X-Men must now band together to save her soul and battle aliens that want to use Grey's new abilities to rule the galaxy.",
+  popularity: 430.768,
+  poster_path: '/kZv92eTc0Gg3mKxqjjDAM73z9cy.jpg',
+  production_companies: [
+    {
+      id: 431,
+      logo_path: null,
+      name: "The Donners' Company",
+      origin_country: 'US'
+    },
+    {
+      id: 25,
+      logo_path: '/qZCc1lty5FzX30aOCVRBLzaVmcp.png',
+      name: '20th Century Fox',
+      origin_country: 'US'
+    },
+    {
+      id: 28788,
+      logo_path: null,
+      name: 'Genre Films',
+      origin_country: 'US'
+    },
+    {
+      id: 9168,
+      logo_path: '/hQNXrYIuyLRxMSBuGHk0GX7CCBS.png',
+      name: 'Bad Hat Harry Productions',
+      origin_country: 'US'
+    },
+    {
+      id: 7505,
+      logo_path: '/837VMM4wOkODc1idNxGT0KQJlej.png',
+      name: 'Marvel Entertainment',
+      origin_country: 'US'
+    },
+    {
+      id: 112049,
+      logo_path: null,
+      name: 'Kinberg Genre',
+      origin_country: ''
+    }
+  ],
+  production_countries: [
+    {
+      iso_3166_1: 'US',
+      name: 'United States of America'
+    }
+  ],
+  release_date: '2019-06-05',
+  revenue: 0,
+  runtime: 114,
+  spoken_languages: [
+    {
+      iso_639_1: 'en',
+      name: 'English'
+    }
+  ],
+  status: 'Released',
+  tagline: 'The phoenix will rise',
+  title: 'Dark Phoenix',
+  video: false,
+  vote_average: 6.2,
+  vote_count: 334,
+  release_dates: {
+    results: [
+      {
+        iso_3166_1: 'ID',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'SK',
+        release_dates: [
+          {
+            certification: '12',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'UA',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'IT',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'ES',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-07T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'CN',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'IE',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-05T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'PL',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-07T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'AU',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'GB',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-05T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'SG',
+        release_dates: [
+          {
+            certification: 'PG13',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-05T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'FI',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-07T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'HU',
+        release_dates: [
+          {
+            certification: '16',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'BR',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'IL',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'CH',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-05T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'HK',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-05T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'DE',
+        release_dates: [
+          {
+            certification: '12',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'TW',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'KR',
+        release_dates: [
+          {
+            certification: '12세 이상 관람가',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-05T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'GR',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'US',
+        release_dates: [
+          {
+            certification: 'PG-13',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-07T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'FR',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-05T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'SE',
+        release_dates: [
+          {
+            certification: '11',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-05T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'JP',
+        release_dates: [
+          {
+            certification: '',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-21T00:00:00.000Z',
+            type: 3
+          }
+        ]
+      },
+      {
+        iso_3166_1: 'RU',
+        release_dates: [
+          {
+            certification: '16+',
+            iso_639_1: '',
+            note: '',
+            release_date: '2019-06-06T00:00:00.000Z',
+            type: 1
+          }
+        ]
+      }
+    ]
+  }
+};
+
+it('should return empty array if release_date prop is not present', () => {
+  const mockMovie = { ...movie };
+  delete mockMovie.release_date;
+  delete mockMovie.release_dates;
+  expect(extractReleaseDatesForRegion(mockMovie)).toHaveLength(0);
+});
+
+it('should return array containing release_date if release_dates prop is not present or release_dates.results array is empty', () => {
+  const mockMovie = { ...movie };
+  mockMovie.release_dates = { results: [] };
+  const defaultReleaseDates = [mockMovie.release_date];
+
+  let releaseDates = extractReleaseDatesForRegion(mockMovie);
+  expect(releaseDates).toHaveLength(defaultReleaseDates.length);
+  expect(releaseDates).toEqual(expect.arrayContaining(defaultReleaseDates));
+
+  delete mockMovie.release_dates;
+  releaseDates = extractReleaseDatesForRegion(mockMovie);
+  expect(releaseDates).toHaveLength(defaultReleaseDates.length);
+  expect(releaseDates).toEqual(expect.arrayContaining(defaultReleaseDates));
+});
+
+it('should return array containing release_date if specified region not found in release_dates.results', () => {
+  const mockMovie = { ...movie };
+  const region = 'US';
+  const defaultReleaseDates = [mockMovie.release_date];
+
+  mockMovie.release_dates = {
+    results: mockMovie.release_dates.results.filter(
+      regionObject => regionObject.iso_3166_1 !== region
+    )
+  };
+
+  const releaseDates = extractReleaseDatesForRegion(mockMovie, region);
+  expect(releaseDates).toHaveLength(defaultReleaseDates.length);
+  expect(releaseDates).toEqual(expect.arrayContaining(defaultReleaseDates));
+});
+
+it('should return array containing all release dates for the specified region', () => {
+  const mockMovie = { ...movie };
+  const region = 'US';
+  const output = ['2019-06-07'];
+
+  const releaseDates = extractReleaseDatesForRegion(mockMovie, region);
+  expect(releaseDates).toHaveLength(output.length);
+  expect(releaseDates).toEqual(expect.arrayContaining(output));
+});
