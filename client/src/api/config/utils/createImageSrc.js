866903993b581942d27404b77d93cbc4c9e8d932
AntoniosProvidakis <ant.providakis@gmail.com>
2019-07-02 19:27:19 +0300

    Refactor createImageSrc.js

--- client/src/api/config/utils/createImageSrc.js
+++ client/src/api/config/utils/createImageSrc.js
@@ -6,56 +6,68 @@ import {
 } from '../constants/image';
 
 export default function createImageSrc({ path, type, size = 'original' }) {
-  let imageSrc;
-
-  try {
-    imageSrc = buildImageUrl({ path, type, size });
-  } catch (error) {
-    console.error(
-      'An error occured while creating image src. Fallback to default base64 image.',
-      error
-    );
-
-    switch (type) {
-      case 'backdrop':
-        imageSrc = defaultBackdropImageBase64Data;
-        break;
-      case 'poster':
-      default:
-        imageSrc = defaultPosterImageBase64Data;
-        break;
-    }
+  if (!isValidPath(path)) {
+    return getDefaultImage(type);
   }
 
-  return imageSrc;
-}
-
-function buildImageUrl({ path, type, size }) {
-  if (path === null || path === undefined) {
-    throw new Error('"path" cannot be null or undefined');
-  }
-
-  if (typeof path !== 'string') {
-    throw new Error('"path" must be a string.');
-  }
-
-  if (path.length === 0 || !path.startsWith('/')) {
-    throw new Error(`"${path}": Check the path`);
-  }
-
-  if (!imageTypes.includes(type)) {
-    throw new Error(
-      `"${type}" image type is not supported. Use one of ${imageTypes}`
-    );
+  if (!isValidType(type, imageTypes)) {
+    return getDefaultImage(type);
   }
 
   const supportedSizes = imageConfig[`${type}_sizes`];
-
-  if (!supportedSizes.includes(size)) {
-    throw new Error(
-      `"${size}" image size is not supported. Use one of ${supportedSizes}`
-    );
+  if (!isValidSize(size, supportedSizes)) {
+    return getDefaultImage(type);
   }
 
   return `${imageConfig.secure_base_url}${size}${path}`;
+}
+
+function getDefaultImage(type) {
+  console.log('Fallback to default base64 image.');
+
+  switch (type) {
+    case 'backdrop':
+      return defaultBackdropImageBase64Data;
+    case 'poster':
+    default:
+      return defaultPosterImageBase64Data;
+  }
+}
+
+function isValidPath(path) {
+  if (typeof path !== 'string') {
+    console.log('"path" must be a string.');
+    return false;
+  }
+
+  if (!path.length) {
+    console.log('"path" cannot be empty string');
+    return false;
+  }
+
+  if (!path.startsWith('/')) {
+    console.log('"path" must start with a /');
+    return false;
+  }
+
+  return true;
+}
+function isValidType(type, supportedTypes) {
+  if (!supportedTypes.includes(type)) {
+    console.log(
+      `"${type}" image type is not supported. Use one of ${supportedTypes}`
+    );
+    return false;
+  }
+  return true;
+}
+
+function isValidSize(size, supportedSizes) {
+  if (!supportedSizes.includes(size)) {
+    console.log(
+      `"${size}" image size is not supported. Use one of ${supportedSizes}`
+    );
+    return false;
+  }
+  return true;
 }
