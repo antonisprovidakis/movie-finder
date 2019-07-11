dde400de9ac223ca9efd0e14d1bec2ad9f698a92
AntoniosProvidakis <ant.providakis@gmail.com>
2019-07-11 16:12:04 +0300

    Minor styles change
    Change styles so that when loading (no main content), the space
    of footer and below it is shown dark.

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -5,9 +5,9 @@ import { Container } from 'semantic-ui-react';
 
 function MainContent({ className = '', children, ...rest }) {
   return (
-    <Container className={`MainContent ${className}`} as="main" {...rest}>
-      {children}
-    </Container>
+    <main className={`MainContent ${className}`} {...rest}>
+      <Container>{children}</Container>
+    </main>
   );
 }
 
